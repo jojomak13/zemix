13674c3053ad6f467902df443a5eb33de70ee0ae
jojomak13 <jojomak350@gmail.com>
2021-01-31 16:00:39 +0200

    final edit in design

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,45 @@
         <h2>Now you can safely say goodbyes to the lengthy delivery processes as you welcome timely delivery with zemix. Have a fast delivery and reap the rewards of loyal customers.</h2>
     </div>
 </header>
+<section class="contact-us">
+    <div class="container">
+        <h2 class="text-center">Contact Us</h2>
+        <div class="row">
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="icon">
+                        <i class="fa fa-phone"></i>
+                    </div>
+                    <a href="tel:+201228967279">+201228967279</a>
+                </div>
+                <div class="card">
+                    <div class="icon">
+                        <i class="fa fa-envelope"></i>
+                    </div>
+                    <a href="mailto:jojomak@gmail.com">jojomak@gmail.com</a>
+                </div>
+                <div class="card">
+                    <div class="icon">
+                        <i class="fa fa-map-marker"></i>
+                    </div>
+                    <a href="javascript:void(0)">Amaadi, Cairo, Egypt</a>
+                </div>
+            </div>
+            <div class="col-md-7">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110502.60379664459!2d31.328332021262092!3d30.05961855887829!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583fa60b21beeb%3A0x79dfb296e8423bba!2z2KfZhNmC2KfZh9ix2KnYjCDZhdit2KfZgdi42Kkg2KfZhNmC2KfZh9ix2KnigKw!5e0!3m2!1sar!2seg!4v1612098477180!5m2!1sar!2seg" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+            </div>
+        </div>
+    </div>
+</section>
+<footer>
+    <div>
+    </div>
+    <div class="copyright text-center">
+        <span>
+            <i class="fa fa-code text-success"></i> with <i class="text-danger fa fa-heart"></i> by <a href="https://www.linkedin.com/in/joseph-makram/" target="_blank">Joseph Makram</a>
+        </span>
+    </div>  
+</footer>
 @endsection
 
 @section('js')
