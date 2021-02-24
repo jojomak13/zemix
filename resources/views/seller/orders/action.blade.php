0f5c8230c281f470e8d255aa4bc66fdf1fa33ebc
jojomak13 <jojomak350@gmail.com>
2021-02-24 14:34:01 +0200

    fix update in seller

--- resources/views/seller/orders/action.blade.php
+++ resources/views/seller/orders/action.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ route('seller.orders.show', $id) }}" class="btn btn-primary"><i class="fa fa-eye"></i></a>
-@if($status_id === 1)
+@if($status_id == 1)
 <a href="{{ route('seller.orders.edit', $id) }}" class="btn btn-warning"><i class="fa fa-edit"></i></a>
 @endif
 @if($notes)
